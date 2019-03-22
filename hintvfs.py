1939397cec4b223c0f8ba4e66b53f2c52017803c
chingwen823 <chingwen823@gmail.com>
2019-03-22 11:34:32 +0800

    modified:   hintvfs.py

--- hintvfs.py
+++ hintvfs.py
@@ -308,7 +308,7 @@ def main():
     alloc_index = -1
     last_node_amount = -1
     data = "**heLLo**" # default data str
-    data_num = -1
+    data_num = 0
 
 
 
@@ -487,7 +487,7 @@ def main():
 
                     vfs_model.send_dummy_pkt(tb)# hacking, send dummy pkt to avoid data lost
                     vfs_model.send_vfs_pkt( NODE_ID, tb, pkt_size, data, data_num, pktno)
-                    logger.info( "pktno:{}\npkt_size{}:\data numer:{}\ndata:{}".format(pktno,pkt_size,data_num,data)) 
+                    logger.info( "===========================\npktno:{}\npkt_size:{}\ndata numer:{}\ndata:{}\n===========================".format(pktno,pkt_size,data_num,data)) 
 
                     if data_num_advance:
                         data_num = data_num + 1 
