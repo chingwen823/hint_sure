7d26d6a28bccc369e51f5465d23497fcd93b3b50
yenmao <teoyenmao@gmail.com>
2019-04-25 17:27:23 +0800

    modified:   hintvfsburn.py

--- hintvfsburn.py
+++ hintvfsburn.py
@@ -543,6 +543,7 @@ def main():
                 if nd_in_response and time.time() > (nd_start_time + time_wait_for_my_slot):
                     if not_my_business: #not my run
                         logger.info( "Not my business")
+                        not_my_business = False
                     else:
                         
                         #prepare data 
